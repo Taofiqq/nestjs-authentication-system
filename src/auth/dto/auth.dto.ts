3ef4f5ba2f39c99f6661e3bbd61516bd6c7f9ea2
Taofiq Aiyelabegan <61933245+Taofiqq@users.noreply.github.com>
2023-05-29 09:57:51 +0100

    addd validation/validation-pipe

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,6 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class SignupDTO {
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 20, {
+    message: 'Password must be between 6 and 20 characters',
+  })
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
   role?: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT;
   await app.listen(port, () => {
     Logger.log(`Server up and running on port ${port}`);
